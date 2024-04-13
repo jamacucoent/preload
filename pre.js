9a3bb6633e7fe18302d1ae00a1c7ba10d2559c62
jamacucoent <166884275+jamacucoent@users.noreply.github.com>
2024-04-13 04:01:52 +0200

    Update pre.js

--- pre.js
+++ pre.js
@@ -10,7 +10,7 @@ overlay.style.cssText = `
     left: 0;
     width: 100%;
     height: 100%;
-    background-color: rgba(255, 255, 255, 0.7);
+    background-color: rgba(232, 243, 246, 0.976);
     z-index: 999;
 `;
 
