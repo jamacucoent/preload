0d178fcac210d74886f065a1967900d3c71fb81b
jamacucoent <166884275+jamacucoent@users.noreply.github.com>
2024-04-14 15:06:41 +0200

    Update pre.js

--- pre.js
+++ pre.js
@@ -3,7 +3,6 @@ const overlay = document.createElement('div');
 overlay.id = 'loader';
 overlay.style.cssText = `
     display: flex;
-    flex-direction: column;
     justify-content: center;
     align-items: center;
     position: fixed;
@@ -11,45 +10,41 @@ overlay.style.cssText = `
     left: 0;
     width: 100%;
     height: 100%;
-    background-color: rgba(232, 243, 246, 0.976);
+    background-color: rgba(255, 255, 255, 0.7);
     z-index: 999;
-`;
-
-const firebaseImage = document.createElement('img');
-firebaseImage.src = 'https://firebasestorage.googleapis.com/v0/b/fliktrip-ff.appspot.com/o/Destinations%20Photos%2Ffliktrip2.png?alt=media&token=bf6a1015-e6fb-4a50-a56a-82369639887f';  // Corrected with quotes around URL
-firebaseImage.style.cssText = `
-    max-width: 50%;  // Adjusted for better visibility
-    max-height: 30%; // Adjusted for better visibility
-    margin-bottom: 20px;
 `;
 
 const svgImage = document.createElement('img');
 svgImage.id = 'svgImage';
 svgImage.src = 'https://cdn.jsdelivr.net/gh/jasminder/flutter_preloader/preload.svg';
 svgImage.style.cssText = `
-    max-width: 100px;  // Adjust size as necessary
-    max-height: 100px; // Adjust size as necessary
+    max-width: 100%;
+    max-height: 100%;
+    display: none;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
 `;
+svgImage.style.display = 'none';
 
-// Append the image and SVG to the overlay
-overlay.appendChild(firebaseImage);
-overlay.appendChild(svgImage);
-
-// Append the overlay to the body
+// Append the elements to the body
 document.body.appendChild(overlay);
+document.body.appendChild(svgImage);
 
-// Function to hide the overlay
+// Function to hide the overlay and display the SVG
 function hideOverlay() {
     overlay.style.display = 'none';
+    svgImage.style.display = 'block';
 }
 
 // Add an event listener to hide the overlay when all external JS files are loaded
 document.addEventListener('DOMContentLoaded', () => {
-    // Simulate a delay for demonstration purposes
-    // This timeout represents the time you assume your external resources take to load
-    setTimeout(hideOverlay, 100);
+    // Replace the following lines with the actual code that loads your external JS files
+    // For demonstration purposes, we'll use a setTimeout to simulate loading external JS files.
+    setTimeout(hideOverlay, 100); // Replace with your actual loading code.
 });
 
-// Fallback: If the DOMContentLoaded event doesn't fire or is not enough,
+// Fallback: If all external resources are loaded and the DOMContentLoaded event doesn't fire,
 // we'll still hide the overlay when the window's load event is triggered.
 window.addEventListener('load', hideOverlay);
