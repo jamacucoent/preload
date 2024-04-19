e5b92b8b377882c8d9b913fe281d30fdbff51a43
jamacucoent <166884275+jamacucoent@users.noreply.github.com>
2024-04-19 13:58:10 +0200

    Update lottie.js

--- lottie.js
+++ lottie.js
@@ -1,4 +1,3 @@
-// Create the overlay and Lottie elements
 const overlay = document.createElement('div');
 overlay.id = 'loader';
 overlay.style.cssText = `
@@ -17,8 +16,8 @@ overlay.style.cssText = `
 const lottieContainer = document.createElement('div');
 lottieContainer.id = 'lottieContainer';
 lottieContainer.style.cssText = `
-    max-width: 20%;
-    max-height: 20%;
+    max-width: 10%;
+    max-height: 10%;
     display: none;
     position: absolute;
     top: 50%;
@@ -26,35 +25,22 @@ lottieContainer.style.cssText = `
     transform: translate(-50%, -50%);
 `;
 
-// Append the elements to the body
 document.body.appendChild(overlay);
 document.body.appendChild(lottieContainer);
 
-// Configure Lottie to load and play animation
-const animation = bodymovin.loadAnimation({
-    container: lottieContainer,
-    renderer: 'svg',
-    loop: true,
-    autoplay: true,
-    path: 'https://lottie.host/a0c44c21-740e-4188-bd5d-78412ef65f70/vCs4Xc7JYs.json' // Replace with your Lottie animation JSON URL
-});
-
-// Function to hide the overlay and display the Lottie animation
 function hideOverlay() {
     overlay.style.display = 'none';
     lottieContainer.style.display = 'block';
 }
 
-// Add an event listener to hide the overlay when all external JS files are loaded
-document.addEventListener('DOMContentLoaded', () => {
-    // Simulate a delay for demonstration purposes
-    setTimeout(hideOverlay, 100); // Replace with your actual loading code.
+window.addEventListener('load', () => {
+    const animation = bodymovin.loadAnimation({
+        container: lottieContainer,
+        renderer: 'svg',
+        loop: true,
+        autoplay: true,
+        path: 'https://lottie.host/a0c44c21-740e-4188-bd5d-78412ef65f70/vCs4Xc7JYs.json'
+    });
+
+    setTimeout(hideOverlay, 100);
 });
-
-// Fallback: If all external resources are loaded and the DOMContentLoaded event doesn't fire,
-// we'll still hide the overlay when the window's load event is triggered.
-window.addEventListener('load', hideOverlay);
-
-// For demonstration purposes, we'll use a setTimeout to simulate loading external resources.
-
-setTimeout(hideOverlay, 2000); // Replace with your actual loading code.
